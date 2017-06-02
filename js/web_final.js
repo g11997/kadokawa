75b31e9609c4e71dbd8879f60b1f897f79aa2737
nankanull <534129087@qq.com>
2017-06-02 21:49:48 +0800

    update

--- js/web_final.js
+++ js/web_final.js
@@ -1,36 +1,17 @@
 $(document).ready(function() {
- 	var bigwidth=screen.width;
-   	console.log(bigwidth);
-   	var imagewidth=document.getElementById(".slick-image");
-   	console.log(imagewidth);
    	$('.ui-slick-main').slick({
-   
-
         infinite: true,
         dots: true,
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         adaptiveHeight: true,
         autoplaySpeed: 2000,
-        centerMode: true,
-        centerPadding: '50px',
+        // centerMode: true,
+        // centerPadding: '50px',
         variableWidth: true,
         arrows: true,
         touchMove: false,
-        responsive: [{
-            breakpoint: 641,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                centerMode: false,
-                infinite: true,
-                dots: true,
-                arrows: false,
-                touchMove: true,
-                variableWidth: false
-            }
-        }]
     }).css('visibility', 'visible');
 
 });
