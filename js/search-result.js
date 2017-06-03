bbcedb5b3597267fabb5fe50d73fa36a988dce42
Colinaya <2296362139@qq.com>
2017-06-03 17:33:57 +0800

    .

--- js/search-result.js
+++ js/search-result.js
@@ -7,10 +7,12 @@ $(".view-increase a").on('click', function () {
 	if ($(this).parent().prev().hasClass('hide')) {
 		$(this).parent().prev().removeClass().addClass('active');
 		$(this).text('-表示を減らす');
-		document.getElementById("ui-leftbody-item2").style.height="500px";
+		document.getElementById("ui-leftbody").style.height="1500px";
+		document.getElementById("ui-leftbody-item2").style.height="557px";
 	} else {
 		$(this).parent().prev().removeClass().addClass('hide');
 		$(this).text('+表示を増やす');
+		document.getElementById("ui-leftbody").style.height="1345px";
 		document.getElementById("ui-leftbody-item2").style.height="402px";
 	}
 	return false;
